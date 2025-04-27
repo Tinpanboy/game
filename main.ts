0d4c4d3fcd7902399cc2ab65bc90401f0528d262
Tinpanboy <bonej2003@gmail.com>
2025-04-27 18:04:52 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+enum RadioMessage {
+    message1 = 49434,
+    hello = 49337
+}
+basic.forever(function () {
+    basic.showArrow(ArrowNames.North)
+    basic.showArrow(ArrowNames.NorthEast)
+    basic.showArrow(ArrowNames.East)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.showArrow(ArrowNames.South)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.showArrow(ArrowNames.West)
+    basic.showArrow(ArrowNames.NorthWest)
+})
